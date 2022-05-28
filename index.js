247e4d10cfc0062c8d2c87e4852917d44f7b2471
CoderDotJs <md.aksh6724@gmail.com>
2022-05-28 12:25:17 +0600

    port

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(cors());
 const authRoutes = require("./routes/authRouts");
 
 app.use("/api/auth", authRoutes);
+app.use("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started on port 5000");
 });
